8a01497f2e366f29c445f732b286625f360945b1
Serzas <j_sergej@yahoo.com>
2019-11-26 16:54:40 +0200

    2.0

--- iban-checker-rest/src/main/java/com/serjer/repo/IbanRepository.java
+++ iban-checker-rest/src/main/java/com/serjer/repo/IbanRepository.java
@@ -11,22 +11,24 @@ import com.serjer.service.IbanService;
 
 @Component
 public class IbanRepository {
-	private final static List<Iban> chekedIbans = new ArrayList<>();
+	private final static List<Iban> checkedIbans = new ArrayList<>();
 	
 	@Autowired
 	private IbanService ibanService;
 	
 	public List<Iban> checkListOfIbans(List<Iban> ibans) {
+		checkedIbans.clear();
         ibans.stream().forEach(i -> {
         	if(ibanService.isValidIban(i.getIban())) {
         		i.setValid(true);
-        		chekedIbans.add(i);
+        		checkedIbans.add(i);
         	} else {
         		i.setValid(false);
-        		chekedIbans.add(i);
+        		checkedIbans.add(i);
         	}
         });
-        return chekedIbans;
+        return checkedIbans;
+       
     }
 
 	
